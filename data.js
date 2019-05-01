2b7218c46392c2ac30549648ce669038d416f297
Justin Torrance <justin.torrance91@gmail.com>
2019-05-01 07:32:17 -0600

    Add data for ranges

---
+++ data.js
@@ -0,0 +1,42 @@
+const ranges = [
+  {
+    "name": "St Elias Mountains",
+    "#_of_tallest_peaks": "19"
+  },
+  {
+    "name": "San Juan Range",
+    "#_of_tallest_peaks": "10"
+  },
+  {
+    "name": "Sierra Nevada",
+    "#_of_tallest_peaks": "9"
+  },
+  {
+    "name": "Sangre de Cristo Range",
+    "#_of_tallest_peaks": "8"
+  },
+  {
+    "name": "Front Range",
+    "#_of_tallest_peaks": "7"
+  },
+  {
+    "name": "Sawatch Range",
+    "#_of_tallest_peaks": "7"
+  },
+  {
+    "name": "Cordillera Neovolcanica",
+    "#_of_tallest_peaks": "6"
+  },
+  {
+    "name": "Wrangell Mountains",
+    "#_of_tallest_peaks": "6"
+  },
+  {
+    "name": "Alaska Range",
+    "#_of_tallest_peaks": "5"
+  },
+  {
+    "name": "other",
+    "#_of_tallest_peaks": "23"
+  }
+]
