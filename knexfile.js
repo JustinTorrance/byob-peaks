54a0deaf37c2d869f96b436ade42e2b51b1e31ca
Justin Torrance <justin.torrance91@gmail.com>
2019-05-03 09:42:31 -0600

    Add production environment

--- knexfile.js
+++ knexfile.js
@@ -9,5 +9,13 @@ module.exports = {
       directory: './db/seeds'
     },
     useNullAsDefault: true
+  },
+  production: {
+    client: 'pg',
+    connection: 'postgres://eyqforjoryddbv:56a3243d5a2df41b937ea3b0f8dd473fb33bbf2a11fe21e14864e2c9f33ff731@ec2-54-225-106-93.compute-1.amazonaws.com:5432/db909mcqtn6kmn',
+    migrations: {
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
   }
 };
