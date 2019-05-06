8d8706d2b2e87829c2ea4b919c1c0130afd686b0
Justin Torrance <justin.torrance91@gmail.com>
2019-05-05 20:15:06 -0600

    Add GET method for ranges by id

--- server.js
+++ server.js
@@ -1,19 +1,38 @@
-
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 const express = require('express');
 const app = express();
+app.use(express.json());
 
 app.set('port', process.env.PORT || 3000)
 
-app.get('/api/v1/peaks', (request, response) => {
+app.get('/api/v1/ranges', (request, response) => {
   database('ranges').select()
-    .then((ranges) => {
-      response.status(200).json(ranges);
+    .then((range) => {
+      response.status(200).json(range);
     })
     .catch((error) => {
       response.status(500).json({ error });
+    });
+});
+
+app.get("/api/v1/ranges/:id", (req, res) => {
+  database("ranges")
+    .where("id", req.params.id)
+    .select()
+    .then(range => {
+      res.status(200).json(range);
+      if (range) {
+        res.status(200).json(range);
+      } else {
+        res.status(404).json({
+          error: `That range does not exist.`
+        });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ error });
     });
 });
 
